6a1bbde50cd90ab6511c9256675fc1dbb64eac0b
Ragavel-Repo-2023 <129276576+Ragavel-Repo-2023@users.noreply.github.com>
2023-04-05 17:13:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int a=12, b=13;
+
+        System.out.println("--Before swap--");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a - b;
+        b = a + b;
+        a = b - a;
+
+        System.out.println("--After swap--");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
